c05c53e614940b59ed066996119413bcde95d8f6
jingjingyu233 <jingjingyu@ucsb.edu>
2020-06-07 18:48:25 -0700

    Updates

---
+++ precache-manifest.d58e1dd03f7a9966c74ec0342a3491a2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa25b0ba953bb50101a195a3b2903038",
+    "url": "/Profile_React/index.html"
+  },
+  {
+    "revision": "537b079328d231d49ca3",
+    "url": "/Profile_React/static/css/2.ccab356c.chunk.css"
+  },
+  {
+    "revision": "4be315ae2794c4cc4f24",
+    "url": "/Profile_React/static/css/main.e2020ef3.chunk.css"
+  },
+  {
+    "revision": "537b079328d231d49ca3",
+    "url": "/Profile_React/static/js/2.e621a7ba.chunk.js"
+  },
+  {
+    "revision": "9a75c9a0d57136cb383fb6572b741bb1",
+    "url": "/Profile_React/static/js/2.e621a7ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4be315ae2794c4cc4f24",
+    "url": "/Profile_React/static/js/main.94cc0026.chunk.js"
+  },
+  {
+    "revision": "97086ca89ad5afa31ded",
+    "url": "/Profile_React/static/js/runtime-main.c7eaf8e7.js"
+  },
+  {
+    "revision": "6b1589e995c27b67429613024f0859d3",
+    "url": "/Profile_React/static/media/JJY1.6b1589e9.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Profile_React/precache-manifest.5c7a402e6f7b13120a09c301206d8952.js"
+  "/Profile_React/precache-manifest.d58e1dd03f7a9966c74ec0342a3491a2.js"
 );
 
 self.addEventListener('message', (event) => {
